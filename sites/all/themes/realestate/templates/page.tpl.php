c34eb95076c4c0742c55b6916f58af0b7ae551f0
Prashant Singhal <prashant.singhal@qed42.com>
2016-04-04 12:59:29 +0530

    Made changes to Features -> About Us, Blogs, Contact, FAQs, Gallery, Home Page, Services and Testimonials and rectified html.tpl.php and page.tpl.php

--- sites/all/themes/realestate/templates/page.tpl.php
+++ sites/all/themes/realestate/templates/page.tpl.php
@@ -1,52 +1,15 @@
-<head>
-<meta name="viewport" content="width = device-width">
-</head>
 <div class="page-wrapper">
-  <div id="header" class="<?php print $secondary_menu ? 'with-secondary-menu': 'without-secondary-menu'; ?>"><div class="section clearfix">
 
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
-    <?php endif; ?>
-
-    <?php if ($site_name || $site_slogan): ?>
-      <div id="name-and-slogan"<?php if ($hide_site_name && $hide_site_slogan) { print ' class="element-invisible"'; } ?>>
-
-        <?php if ($site_name): ?>
-          <?php if ($title): ?>
-            <div id="site-name"<?php if ($hide_site_name) { print ' class="element-invisible"'; } ?>>
-              <strong>
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-              </strong>
-            </div>
-          <?php else: /* Use h1 when the content title is empty */ ?>
-            <h1 id="site-name"<?php if ($hide_site_name) { print ' class="element-invisible"'; } ?>>
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </h1>
-          <?php endif; ?>
-        <?php endif; ?>
-
-        <?php if ($site_slogan): ?>
-          <div id="site-slogan"<?php if ($hide_site_slogan) { print ' class="element-invisible"'; } ?>>
-            <?php print $site_slogan; ?>
-          </div>
-        <?php endif; ?>
-
-      </div> <!-- /#name-and-slogan -->
-    <?php endif; ?>
-  </div></div>
-
-
+<!-- ======= Rendering Regions ========= -->
 
    <div class="header-top">
     <?php print render($page['header_top']); ?>
   </div>
   <div class="header">
     <div class="header-nav">
-    <?php print render($page['header']); ?>
+      <?php print render($page['header']); ?>
+    </div>
   </div>
-  </div></div>
   <div class="content">
     <?php if ($title): ?>
         <h2>
@@ -54,7 +17,6 @@
            <?php endif; ?>
            </h2>
     <?php print render($page['content']); ?>
-
   </div>
   <div class="footer">
   <div class="footer-tags">
@@ -63,7 +25,4 @@
   <div class="preface">
     <?php print render($page['preface']); ?>
   </div>
-
-
-
-</div>
+</div> <!-- /.page-wrapper -->
