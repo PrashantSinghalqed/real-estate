7e968cdad50502f8fdbefc64b4d2c55d02eaf828
rahul <rahulsavaria@gmail.com>
2016-04-13 13:37:18 +0530

    adding menu

---
+++ sites/all/themes/_backup/js/common.js
@@ -0,0 +1,5 @@
+(document).scroll(function() {
+  var h=$(".header-top").height();
+  alert(h);
+  /* Act on the event */
+});
---
+++ sites/all/themes/realestate/js/realestate.js
@@ -0,0 +1,20 @@
+;(function($){
+  "use strict";
+    $(document).ready(function(){
+
+  // alert("hello");
+    $(window).scroll(function(){
+       var h= $(".header-top").height();
+      var st = $(this).scrollTop();
+      if (st > h){
+             $(".header-nav").css("position","fixed").css("top","0px");
+          }
+      else{
+            $(".header-nav").css("position","relative").css("top","0px");
+      }
+    });
+});// your code
+})(jQuery);
+
+
+
