cf51cb2c6de4abe864177f5d2f20f6ac2d262b19
Prashant Singhal <prashant.singhal@qed42.com>
2016-04-20 18:47:01 +0530

    Feature and theme update

--- sites/all/themes/realestate/js/realestate.js
+++ sites/all/themes/realestate/js/realestate.js
@@ -1,35 +1,49 @@
-;(function($){
+(function($){
   "use strict";
     $(document).ready(function(){
+      // parallax script
+  //      var windowEl = $(window);
+  // $('div[data-type="background"]').each(function(){
+  //       var $bgobj = $(this); // assigning the object
 
-   //alert("hello");
-    $(window).scroll(function(){
-      if($(window).width() >= 1024){
-       var h= $(".header-top").height();
-      var st = $(this).scrollTop();
-      if (st > h){
-             $(".header-nav").css("position","fixed").css("top","0px");
-          }
-      else{
-            $(".header-nav").css("position","relative");
-      }
-    }
-    });
-      $(".mobile").click(function(){
-        var n=$("#block-nice-menus-1").css("display");
-        if(n === 'none'){
-         // alert(n);
-          //$("#block-nice-menus-1").css("display","block !important");
-          document.querySelector('style').textContent+="@media (max-width: 767px){.region-header > #block-nice-menus-1{ display:block !important}";
-        }
+  //       $(window).scroll(function() {
+  //           var yPos = -(windowEl.scrollTop() / $bgobj.data('speed'));
+
+  //           // Put together our final background position
+  //           var coords = '50% '+ yPos + 'px';
+
+  //           // Move the background
+  //           $bgobj.css({ backgroundPosition: coords });
+  //       });
+  //   });
+     //alert("hello");
+      $(window).scroll(function(){
+        if($(window).width() >= 1024){
+         var h= $(".header-top").height();
+        var st = $(this).scrollTop();
+        if (st > h){
+               $(".header-nav").css("position","fixed").css("top","0px");
+            }
         else{
-        //alert(n);
-        // alert("The paragraph was clicked.");
-        // $("#block-nice-menus-1").removeClass("block");
-          document.querySelector('style').textContent+="@media (max-width: 767px){.region-header > #block-nice-menus-1{ display:none !important}";
+              $(".header-nav").css("position","relative");
         }
+      }
       });
-  });// your code
+        $(".mobile").click(function(){
+          var n=$("#block-nice-menus-1").css("display");
+          if(n === 'none'){
+           // alert(n);
+            //$("#block-nice-menus-1").css("display","block !important");
+            document.querySelector('style').textContent+="@media (max-width: 767px){.region-header > #block-nice-menus-1{ display:block !important}";
+          }
+          else{
+          //alert(n);
+          // alert("The paragraph was clicked.");
+          // $("#block-nice-menus-1").removeClass("block");
+            document.querySelector('style').textContent+="@media (max-width: 767px){.region-header > #block-nice-menus-1{ display:none !important}";
+          }
+        });
+    });// your code
 })(jQuery);
 
 
