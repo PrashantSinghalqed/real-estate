e530c2d1b8e3010ab85ad405c08e5aa91ab8ce61
Prashant Singhal <prashant.singhal@qed42.com>
2016-04-20 19:14:24 +0530

    Feature and theme changes; merging conflicts

--- sites/all/themes/realestate/js/realestate.js
+++ sites/all/themes/realestate/js/realestate.js
@@ -5,7 +5,6 @@
   //      var windowEl = $(window);
   // $('div[data-type="background"]').each(function(){
   //       var $bgobj = $(this); // assigning the object
-
   //       $(window).scroll(function() {
   //           var yPos = -(windowEl.scrollTop() / $bgobj.data('speed'));
 
@@ -16,34 +15,42 @@
   //           $bgobj.css({ backgroundPosition: coords });
   //       });
   //   });
-     //alert("hello");
-      $(window).scroll(function(){
-        if($(window).width() >= 1024){
-         var h= $(".header-top").height();
-        var st = $(this).scrollTop();
-        if (st > h){
-               $(".header-nav").css("position","fixed").css("top","0px");
-            }
-        else{
-              $(".header-nav").css("position","relative");
-        }
+   //alert("hello");
+    $(window).scroll(function(){
+      if($(window).width() >= 1024){
+       var h= $(".header-top").height();
+      var st = $(this).scrollTop();
+      if (st > h){
+             $(".header-nav").css("position","fixed").css("top","0px");
+          }
+      else{
+            $(".header-nav").css("position","relative");
       }
+    }
+    });
+      $(".mobile").click(function(){
+        var n=$("#block-nice-menus-1").css("display");
+        if(n === 'none'){
+
+          //$("#block-nice-menus-1").css("display","block !important");
+          document.querySelector('style').textContent+="@media (max-width: 767px){.region-header > #block-nice-menus-1{ display:block !important}";
+        }
+        else{
+              document.querySelector('style').textContent+="@media (max-width: 767px){.region-header > #block-nice-menus-1{ display:none !important}";
+        }
       });
-        $(".mobile").click(function(){
-          var n=$("#block-nice-menus-1").css("display");
-          if(n === 'none'){
-           // alert(n);
-            //$("#block-nice-menus-1").css("display","block !important");
-            document.querySelector('style').textContent+="@media (max-width: 767px){.region-header > #block-nice-menus-1{ display:block !important}";
-          }
-          else{
-          //alert(n);
-          // alert("The paragraph was clicked.");
-          // $("#block-nice-menus-1").removeClass("block");
-            document.querySelector('style').textContent+="@media (max-width: 767px){.region-header > #block-nice-menus-1{ display:none !important}";
-          }
-        });
-    });// your code
+      $('.block-block-2 p').click(function(){
+        if($("#block-search-form").hasClass('active')){
+          $("#block-search-form").removeClass('active');
+
+        // $("#block-search-form").animate({width:'toggle'},400);
+        // $(".form-type-textfield").toggle(500);
+        }
+        else{
+          $("#block-search-form").addClass('active');
+        }
+      });
+  });// your code
 })(jQuery);
 
 
